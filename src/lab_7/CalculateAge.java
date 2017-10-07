c8ad55a2c7926e07903b4e8d68f724f63558cf42
Eazymov <eazymov@mail.ru>
2017-10-07 16:02:37 +0500

    updated

--- src/lab_7/CalculateAge.java
+++ src/lab_7/CalculateAge.java
@@ -1,21 +1,51 @@
 package lab_7;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class CalculateAge {
 	
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		String birthdate, name;
+		int age;
+
+		System.out.println("Введите свое имя или дату рождения: ");
+		String value = sc.nextLine();
+		
+		if (isNumber(value)) {
+			birthdate = value;
+			age = getAgeFromBirthday(birthdate);
+
+			System.out.println("Вам " + age + " лет. Введите свое имя: ");
+			name = sc.nextLine();
+			System.out.println("Здравствуйте, " + name + ".");
+		} else {
+			name = value;
+
+			System.out.println("Здравствуйте, " + name + ". Введите свою дату рождения: ");
+			birthdate = sc.nextLine();
+			age = getAgeFromBirthday(birthdate);
+			System.out.println("Вам " + age + " лет.");
+		}
+	}
+	
+	private static int getAgeFromBirthday(String birthdate) {
+		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+		int birthYear = Integer.parseInt(birthdate);
+		
+		return currentYear - birthYear;
+	}
+	
+	private static boolean isNumber(String value) {
 		try {
-			Scanner sc = new Scanner(System.in);
-			System.out.print("Р’РІРµРґРёС‚Рµ СЃРІРѕРµ РёРјСЏ: ");
-			String name = sc.nextLine();
-			System.out.println("РџСЂРёРІРµС‚ " + name + "! РЈРєР°Р¶Рё СЃРІРѕР№ РіРѕРґ СЂРѕР¶РґРµРЅРёСЏ:");
-			String age = sc.nextLine();
+			int number = Integer.parseInt(value);
+			
+			return true;
 
 		} catch (Throwable t) {
-			System.out.print(t.getMessage());
-			t.printStackTrace();
+			return false;
 		}
 	}
 }
